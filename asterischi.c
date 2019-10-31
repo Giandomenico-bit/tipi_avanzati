ad84361cd9b07e3c216a96e9532c37f04799a28b
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2019-10-31 23:39:18 +0100

    asterischi.c

---
+++ asterischi.c
@@ -0,0 +1,23 @@
+/* Asterischi
+
+Si scriva un programma che stampi 3
+asterischi (uno per riga) a
+distanza di 700 millisecondi l'uno dall'altro. */
+
+#include <stdio.h>
+#include <time.h>
+
+int main( void ){
+
+  typedef struct timespec time_s;
+  time_s millis;
+
+  millis.tv_sec = 700 * 1000;
+  printf( "*\n" );
+  millis.tv_sec = 700 * 1000;
+  printf( "*\n" );
+  millis.tv_sec = 700 * 1000;
+  printf( "*\n" );
+
+  return 0;
+}
