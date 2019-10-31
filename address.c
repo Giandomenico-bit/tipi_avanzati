4d8209f913be45932be12c17df6be0a107874b0c
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2019-10-31 23:27:06 +0100

    address.c

---
+++ address.c
@@ -0,0 +1,19 @@
+/* Address
+
+Si scriva un programma C che stampi
+il valore dell'indirizzo di una variabile
+float dopo averlo memorizzato in un puntatore. */
+
+#include <stdio.h>
+
+int main( void ){
+
+  float* floatPtr;
+  float number;
+
+  floatPtr = &number;
+
+  printf( "Valore puntatore . %p\n", floatPtr );
+  
+  return 0;
+}
