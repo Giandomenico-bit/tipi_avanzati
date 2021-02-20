a3548b7b96c4b58152f16e310401e45e71e81167
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2021-02-20 23:54:06 +0100

    Update

--- asterischi.c
+++ asterischi.c
@@ -1,25 +1,25 @@
 /* Asterischi
 
 Si scriva un programma che stampi 3
-asterischi (uno per riga) a
+asterischi ( uno per riga ) a
 distanza di 700 millisecondi l'uno dall'altro. */
 
 #include <stdio.h>
 #include <time.h>
 
-int main( void ){
+int main( void ) {
 
-  typedef struct timespec tempo;
-  tempo milliSecondi;
+  struct timespec intervallo;
 
-  milliSecondi.tv_nsec = 700 * 1000 * 1000; // 700 millisecondi
+  intervallo.tv_sec = 0;
+  intervallo.tv_nsec = 700 * 1000 * 1000;
 
-  puts( "*" );
-  nanosleep( &milliSecondi, NULL );
-  puts( "*" );
-  nanosleep( &milliSecondi, NULL );
-  puts( "*" );
-  nanosleep( &milliSecondi, NULL );
+  printf( "%c\n", '*' );
+  nanosleep( &intervallo, NULL );
+  printf( "%c\n", '*' );
+  nanosleep( &intervallo, NULL );
+  printf( "%c\n", '*' );
+  nanosleep( &intervallo, NULL );
 
   return 0;
 }
